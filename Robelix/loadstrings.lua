3461c4f7eca9d2f39b47613672787c99ac4d7c9b
Empire <104152235+Empire4946@users.noreply.github.com>
2022-06-27 18:37:01 +0800

    fix

---
+++ Robelix/loadstrings.lua
@@ -0,0 +1,18 @@
+--[[
+██████╗  ██████╗ ██████╗ ███████╗██╗     ██╗██╗  ██╗   ██████╗ ██╗   ██╗██████╗ ██╗     ██╗ ██████╗
+██╔══██╗██╔═══██╗██╔══██╗██╔════╝██║     ██║╚██╗██╔╝██╗██╔══██╗██║   ██║██╔══██╗██║     ██║██╔════╝
+██████╔╝██║   ██║██████╔╝█████╗  ██║     ██║ ╚███╔╝ ╚═╝██████╔╝██║   ██║██████╔╝██║     ██║██║     
+██╔══██╗██║   ██║██╔══██╗██╔══╝  ██║     ██║ ██╔██╗ ▄█╗██╔═══╝ ██║   ██║██╔══██╗██║     ██║██║     
+██║  ██║╚██████╔╝██████╔╝███████╗███████╗██║██╔╝ ██╗▀═╝██║     ╚██████╔╝██████╔╝███████╗██║╚██████╗
+╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝╚══════╝╚═╝╚═╝  ╚═╝   ╚═╝      ╚═════╝ ╚═════╝ ╚══════╝╚═╝ ╚═════╝
+
+Public Release
+ver: 1
+
+Features:
+No more 60fps limit thus giving you more fps
+Discord Invite Disabled due to misunderstanding reasons
+]]
+
+_G.fpscap = "500" -- Do NOT put it extremely high cause it will just crash your game.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Empire4946/Robelix/main/Robelix/script.txt"))()

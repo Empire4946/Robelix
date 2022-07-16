a66ea615c7306e6ff09dd5793a458a8eebd6070e
Empire <104152235+Empire4946@users.noreply.github.com>
2022-07-16 19:30:08 +0800

    Update loadstrings xd
    xd

--- Robelix/loadstrings.lua
+++ Robelix/loadstrings.lua
@@ -7,12 +7,13 @@
 ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝╚══════╝╚═╝╚═╝  ╚═╝   ╚═╝      ╚═════╝ ╚═════╝ ╚══════╝╚═╝ ╚═════╝
 
 Public Release
-ver: 1
+ver: 1.2
 
 Features:
-No more 60fps limit thus giving you more fps
-Discord Invite Disabled due to misunderstanding reasons
+Sections
+Lighting Changer
+
+BIG UPDATE
 ]]
 
-_G.fpscap = "500" -- Do NOT put it extremely high cause it will just crash your game.
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Empire4946/Robelix/main/Robelix/script.txt"))()
